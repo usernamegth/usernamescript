2bd72404bcfce4fcdf89d51f788484bd06b321c8
usernamegth <112678550+usernamegth@users.noreply.github.com>
2022-09-02 15:01:10 +1000

    Update username.sh

--- username.sh
+++ username.sh
@@ -2,5 +2,7 @@ pass=CyberTaipan123!
 ufw enable
 echo "Firewall Enabled."
 while IFS= read -r line; do
-    echo "Text read from file: $line"
+    echo "$line"
+    if grep *$line* /etc/passwd = $line then
+        echo "uit worked"
 done < users.txt
