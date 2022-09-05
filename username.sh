223bf429bdf72eb5919cca4c7c802499d809203d
usernamegth <112678550+usernamegth@users.noreply.github.com>
2022-09-05 14:18:16 +1000

    Update username.sh

--- username.sh
+++ username.sh
@@ -1,8 +1,7 @@
 pass=CyberTaipan123!
 ufw enable
 echo "Firewall Enabled."
-while IFS= read -r line; do
-    echo "$line"
-    if grep *$line* /etc/passwd = $line then
-        echo "uit worked"
-done < users.txt
+#Editing the login.defs file. 
+sed -i '160s/.*/PASS_MAX_DAYS   30/' /etc/login.defs
+sed -i '161s/.*/PASS_MIN_DAYS   5/' /etc/login.defs
+sed -i '162s/.*/PASS_WARN_AGE   7/' /etc/login.defs
